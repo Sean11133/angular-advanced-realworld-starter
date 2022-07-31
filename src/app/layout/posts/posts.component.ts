ae22438a3233823d6875bf901beb68327c454780
hycheng5 <hycheng5@winbond.com>
2022-07-31 13:09:25 +0800

    串接文章列表 API

--- src/app/layout/posts/posts.component.ts
+++ src/app/layout/posts/posts.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Articles } from 'src/app/interfaces/articles';
+import { PostService } from 'src/app/post.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
 export class PostsComponent implements OnInit {
+  articlesData: Articles = {
+    articles: [],
+  };
 
-  constructor() { }
+  constructor(private post: PostService) {}
 
   ngOnInit(): void {
+    this.getArticles();
   }
 
+  getArticles() {
+    this.post.getArticles().subscribe((s) => {
+      this.articlesData = s;
+    });
+  }
 }
