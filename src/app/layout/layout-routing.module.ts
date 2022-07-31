b2fc7d4a87eeb0c3ad5a98589db28a5432786291
hycheng5 <hycheng5@winbond.com>
2022-07-31 09:47:47 +0800

    add posts

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout.component';
 
-const routes: Routes = [{ path: '', component: LayoutComponent }];
+const routes: Routes = [
+  { path: '', component: LayoutComponent },
+  {
+    path: 'posts',
+    loadChildren: () =>
+      import('./posts/posts.module').then((m) => m.PostsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
