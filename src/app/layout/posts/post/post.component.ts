d5f2df9063a3c64301026b094720f497fc5782c3
hycheng5 <hycheng5@winbond.com>
2022-07-31 13:09:57 +0800

    串接單篇文章API

--- src/app/layout/posts/post/post.component.ts
+++ src/app/layout/posts/post/post.component.ts
@@ -1,14 +1,42 @@
+import { PostService } from './../../../post.service';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Article } from 'src/app/interfaces/article';
 
 @Component({
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
-
-  constructor() { }
+  id: string | null = '';
+  article: Article = {
+    id: '',
+    title: '',
+    description: '',
+    body: '',
+    tagList: [],
+    createdAt: '',
+    updatedAt: '',
+    author: '',
+  };
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private post: PostService
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = params.get('id');
+
+      this.getPost(this.id);
+    });
   }
 
+  getPost(id: string | null) {
+    this.post.getArticle(id || '').subscribe((s) => {
+      this.article = s.article;
+      console.log(s);
+    });
+  }
 }
